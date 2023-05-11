14d1961d7c0d2620c31278239fee1693a26d4ac5
stevehotcodes <stevehotcodes@gmail.com>
2023-05-11 17:01:27 +0300

    contains express setup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express, {json} from 'express'
 
+import productRouter from './routes/productRoutes'
+
 
 const app=express()
 app.use(json())// middleware
+
+app.use('/products',productRouter)
+// app.use('/allProducts', productRouter)
 
 
 app.listen(4000, ()=>{
