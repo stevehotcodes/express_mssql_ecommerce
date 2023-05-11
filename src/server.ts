c3b9dd78bcbd3dd1d011759bbe2a6378ceda2c3c
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-11 20:07:13 +0300

    import and use user routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express, {json} from 'express'
-
+import userRoutes from './routers/userRoutes'
 
 const app=express()
 app.use(json())// middleware
-
+app.use('/users', userRoutes)
 
 app.listen(4000, ()=>{
     console.log("Server Running...")
