286fa3c6fd03277443817b14dd086b9ed85cf354
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-15 01:11:46 +0300

    Add cart stored procedures

---
+++ DATABASE/stored_procedures/cart/addCartItem.sql
@@ -0,0 +1,12 @@
+CREATE OR ALTER PROCEDURE addCartItem(
+@id VARCHAR(200),
+@productID VARCHAR(200),
+@quantity INT,
+@userID VARCHAR(200)
+)
+AS
+BEGIN
+INSERT INTO cart (id, productID, quantity, userID)
+VALUES (@id, @productID, @quantity, @userID)
+END
+
