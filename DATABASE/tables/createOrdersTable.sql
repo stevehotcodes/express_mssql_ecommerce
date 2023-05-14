9d588218caffef07d94fd16d6958867f6b489637
stevehotcodes <stevehotcodes@gmail.com>
2023-05-14 17:19:23 +0300

    recreated orders and sales table

--- DATABASE/tables/createOrdersTable.sql
+++ DATABASE/tables/createOrdersTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE orders(
 id VARCHAR(200) PRIMARY KEY,
-orderStatus VARCHAR(200) DEFAULT 'processing',
-orderDate GETDATE() ,
-userID  VARCHAR(200) FOREIGN KEY REFERENCES users(id)
+orderStatus VARCHAR(200) DEFAULT 'processing' CHECK (orderStatus IN ('processing','shipping','shipped', 'canceled')),
+orderDate DATETIME DEFAULT GETDATE(),
+userID  VARCHAR(200) 
+ FOREIGN KEY REFERENCES users(id)
 )
-
