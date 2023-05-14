1f34f7c06d3874ad17d3c5f3a94e7ae807e3d184
stevehotcodes <stevehotcodes@gmail.com>
2023-05-14 16:15:09 +0300

    resolved spelling, added price column and removed the redundant statement

--- DATABASE/tables/createOrdersTable.sql
+++ DATABASE/tables/createOrdersTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE orders(
 id VARCHAR(200) PRIMARY KEY,
 orderStatus VARCHAR(200) DEFAULT 'processing',
-orderDate DATE ,
+orderDate GETDATE() ,
 userID  VARCHAR(200) FOREIGN KEY REFERENCES users(id)
 )
 
