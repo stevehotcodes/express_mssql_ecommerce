3e51054c01e571c201aa23ad6ed91c80e2a0388e
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-15 12:07:21 +0300

    minor change

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express, {json} from 'express'
 import userRoutes from './routers/userRoutes'
 import productRouter from './routes/productRoutes'
+import cartRoutes from './routers/cartRoutes'
 
 const app=express()
 app.use(json())// middleware
 app.use('/users', userRoutes)
 app.use('/products',productRouter)
-// app.use('/allProducts', productRouter)
+app.use('/cart', cartRoutes)
 
 
 app.listen(4000, ()=>{
-    console.log("Server Running...")
+    console.log("✔️ Server Running... 🚀")
 })
