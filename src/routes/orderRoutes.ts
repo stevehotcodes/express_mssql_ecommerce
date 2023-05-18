680dc30a19ebfc7cd0dd7133be28e2a9ac2c1e47
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-18 18:30:27 +0300

    protect order routes

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { cancelOrderById, createNewOrder,  getAllOrders, getAnOrderById, getAnOrderByStatus, getOrdersByUser } from "../controllers/orderController";
-import { accountRequired } from "../middlewares/auth";
+import { accountRequired, adminPrivilage } from "../middlewares/auth";
 
 const orderRoutes=Router();
 
-orderRoutes.post("", accountRequired,createNewOrder)
-orderRoutes.get("", accountRequired,getOrdersByUser)
-orderRoutes.post("/cancel/:id",accountRequired,cancelOrderById)
+orderRoutes.post("", accountRequired, createNewOrder)
+orderRoutes.get("", accountRequired, getOrdersByUser)
+orderRoutes.patch("/cancel/:id", accountRequired, cancelOrderById)
 orderRoutes.get("/:id",accountRequired,getAnOrderById)
 // orderRoutes.patch("/:id",accountRequired,getAnOrderById)
 orderRoutes.post("/status",accountRequired,getAnOrderByStatus)
-orderRoutes.get("/all ", accountRequired,getAllOrders)
+orderRoutes.get("/all ", adminPrivilage, getAllOrders)
 
 export default orderRoutes
