a4044d059d1d5811a40e86e775ff1fb145ab7c80
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-31 17:46:12 +0300

    not null constraints for required columns

--- DATABASE/tables/createOrdersTable.sql
+++ DATABASE/tables/createOrdersTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE orders(
 id VARCHAR(200) PRIMARY KEY,
 orderStatus VARCHAR(200) DEFAULT 'processing' CHECK (orderStatus IN ('processing','shipping','shipped', 'canceled')),
 orderDate DATETIME DEFAULT GETDATE(),
-userID  VARCHAR(200) 
+userID  VARCHAR(200) NOT NULL
  FOREIGN KEY REFERENCES users(id)
 )
